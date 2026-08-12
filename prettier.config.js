91e11bd2f93b0c6fac124533ed2112f0ac73fe77
Felipe Nascimento <nctfelipe@gmail.com>
2026-08-12 11:29:06 -0300

    chore: configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
