b834277ef955367d04066bb350ad3538646a8d27
Felipe Nascimento <nctfelipe@gmail.com>
2026-08-12 11:29:06 -0300

    chore: enable strict type-aware ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -11,12 +11,16 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
     },
   },
 ]);
